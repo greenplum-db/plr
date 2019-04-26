6ef46d871c5dc9a1b1e1695094f84a7b8c670e30
Hao Wu <gfphoenix78@gmail.com>
2019-04-26 15:43:34 +0800

    ubuntu16

--- concourse/scripts/build_plr.sh
+++ concourse/scripts/build_plr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
 
+# OSVER : centos6, centos7, ubuntu18
+
 set -exo pipefail
 
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -7,14 +9,32 @@ TOP_DIR=${CWDIR}/../../../
 
 source "${TOP_DIR}/gpdb_src/concourse/scripts/common.bash"
 
+function install_R() {
+case $OSVER in
+centos*)
+	yum install -y R
+	export R_HOME=/usr/lib64/R
+	;;
+ubuntu*)
+	apt update
+	apt install -y r-base
+	export R_HOME=/usr/lib/R
+	;;
+*)
+	echo "unknown OS = $OS"
+	;;
+esac
+}
+
 function pkg() {
     ## Install R before source greenplum_path
-    yum install -y R
+    install_R
+    which R
+    echo "R_HOME='$R_HOME'"
     
-	source /opt/gcc_env.sh
+    source /opt/gcc_env.sh
     source /usr/local/greenplum-db-devel/greenplum_path.sh
 
-    export R_HOME=/usr/lib64/R
     export USE_PGXS=1
     pushd plr_src/src
     make clean
