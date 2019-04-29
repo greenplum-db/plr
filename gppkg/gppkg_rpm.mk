4b567167b88c513e32d912326b078be1accde810
Hao Wu <gfphoenix78@gmail.com>
2019-04-29 08:44:06 +0000

    fix rpm

--- gppkg/gppkg_rpm.mk
+++ gppkg/gppkg_rpm.mk
@@ -9,35 +9,36 @@ ARCH=$(shell uname -p)
 RPM_ARGS=$(subst -, ,$*)
 RPM_NAME=$(word 1,$(RPM_ARGS))
 PLR_RPM=plr-$(PLR_VER)-$(PLR_REL).$(ARCH).rpm
-PLR_RPM_FLAGS="--define 'plr_dir $(PLR_DIR)/src' --define 'plr_ver $(PLR_VER)' --define 'plr_rel $(PLR_REL)' --define 'r_ver $(R_VER)' --define 'r_dir $(R_HOME)'"
+PLR_RPM_FLAGS= --define 'plr_dir $(PLR_DIR)/src' --define 'plr_ver $(PLR_VER)' --define 'plr_rel $(PLR_REL)' --define 'r_ver $(R_VER)' --define 'r_dir $(R_HOME)'
+TARGET_GPPKG=plr-$(PLR_VER).$(PLR_REL)-$(ARCH).gppkg
 PWD=$(shell pwd)
 
 .PHONY: distro
 distro: $(TARGET_GPPKG)
 
-%.rpm: 
+%.rpm:
+	echo "PLR=$(PLR_DIR)"
 	rm -rf RPMS BUILD SPECS
 	mkdir RPMS BUILD SPECS
 	cp $(RPM_NAME).spec SPECS/
-	rpmbuild -bb SPECS/$(RPM_NAME).spec --buildroot $(PWD)/BUILD --define '_topdir $(PWD)' --define '__os_install_post \%{nil}' --define 'buildarch $(ARCH)' $(RPM_FLAGS)
+	rpmbuild -bb SPECS/$(RPM_NAME).spec --buildroot $(PWD)/BUILD --define '_topdir $(PWD)' --define '__os_install_post \%{nil}' --define 'buildarch $(ARCH)' $(PLR_RPM_FLAGS)
 	mv RPMS/$(ARCH)/$*.rpm .
-	rm -rf RPMS BUILD SPECS
+	#rm -rf RPMS BUILD SPECS
 
-%.gppkg: $(MAIN_RPM) $(DEPENDENT_RPMS)
-	mkdir -p gppkg/deps 
+%.gppkg: $(PLR_RPM) $(DEPENDENT_RPMS)
+	mkdir -p gppkg/deps
 	cp gppkg_spec.yml gppkg/
-	cp $(MAIN_RPM) gppkg/ 
+	cp $(PLR_RPM) gppkg/
 ifdef DEPENDENT_RPMS
 	for dep_rpm in $(DEPENDENT_RPMS); do \
 		cp $${dep_rpm} gppkg/deps; \
 	done
 endif
-	source $(GPHOME)/greenplum_path.sh && gppkg --build gppkg 
+	source $(GPHOME)/greenplum_path.sh && gppkg --build gppkg
 
 clean:
-	rm -rf RPMS BUILD SPECS
+	rm -rf RPMS BUILD SPECS SOURCES SRPMS
 	rm -rf gppkg
-	rm -f gppkg_spec.yml
 ifdef EXTRA_CLEAN
 	rm -f $(EXTRA_CLEAN)
 endif
