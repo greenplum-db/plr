e4cd58dfa17f78f86ab2b25588f96dca43657b2c
yihong0618 <zouzou0208@gmail.com>
2022-03-25 15:09:37 +0800

    feat: ci for mirrorless test branch

--- concourse/scripts/test_plr.sh
+++ concourse/scripts/test_plr.sh
@@ -29,7 +29,6 @@ EOF
     chown -R gpadmin:gpadmin $(pwd)
     chown gpadmin:gpadmin /home/gpadmin/test.sh
     chmod a+x /home/gpadmin/test.sh
-
 }
 
 function test() {
@@ -73,7 +72,10 @@ function _main() {
     time install_pkg
     time install_gpdb
     time setup_gpadmin_user
-
+    # for mirrorless test
+    export WITH_MIRRORS=false
+    export WITH_STANDBY=false
+    export BLDWRAP_POSTGRES_CONF_ADDONS=wal_level=minimal,max_wal_senders=0,gp_dispatch_keepalives_idle=30,gp_dispatch_keepalives_interval=10,gp_dispatch_keepalives_count=4
     time make_cluster
     time prepare_test
     time test
