f1d956619e6ebd93fe6e5c6cc6de8864a5f243eb
Xuebin Su <sxuebin@vmware.com>
2023-02-28 10:35:49 +0800

    Use different schedules for GP 6X and 7X

--- concourse/scripts/test_plr.sh
+++ concourse/scripts/test_plr.sh
@@ -15,7 +15,13 @@ function _main() {
     gpstop -ra
 
     pushd /home/gpadmin/plr_src/src
-    time make USE_PGXS=1 installcheck
+
+    MAJOR_VERSION = $(shell grep GP_MAJORVERSION $(shell $(PG_CONFIG) --includedir-server)/pg_config.h | grep -Eo '[0-9]+')
+    ifeq ($(MAJOR_VERSION), 7)
+        time make USE_PGXS=1 REGRESS_OPTS+=' --schedule=../regress/plr_schedule_7X' installcheck
+    else
+        time make USE_PGXS=1 REGRESS_OPTS+=' --schedule=../regress/plr_schedule' installcheck
+    endif
     popd
 }
 
