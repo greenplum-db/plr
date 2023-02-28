b004b78e528e0ed8cf4769e1c938399fcfd7cf98
Xuebin Su <sxuebin@vmware.com>
2023-02-28 11:32:18 +0800

    Change REGRESS_OPTS in Makefile

--- concourse/scripts/test_plr.sh
+++ concourse/scripts/test_plr.sh
@@ -16,12 +16,7 @@ function _main() {
 
     pushd /home/gpadmin/plr_src/src
 
-    MAJOR_VERSION = $(shell grep GP_MAJORVERSION $(shell $(PG_CONFIG) --includedir-server)/pg_config.h | grep -Eo '[0-9]+')
-    ifeq ($(MAJOR_VERSION), 7)
-        time make USE_PGXS=1 REGRESS_OPTS+=' --schedule=../regress/plr_schedule_7X' installcheck
-    else
-        time make USE_PGXS=1 REGRESS_OPTS+=' --schedule=../regress/plr_schedule' installcheck
-    endif
+    time make USE_PGXS=1 installcheck
     popd
 }
 
