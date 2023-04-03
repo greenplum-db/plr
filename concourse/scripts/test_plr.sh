5a7690d626357686e1b49b917887f69c95b9a8be
Chen Mulong <chenmulong@gmail.com>
2023-04-03 15:19:00 +0800

    test

--- concourse/scripts/test_plr.sh
+++ concourse/scripts/test_plr.sh
@@ -5,28 +5,41 @@ set -exo pipefail
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP_DIR=${CWDIR}/../../../
 
+function install_gppkg() {
+    if [[ ${GP_MAJOR_VERSION} == "7" ]]; then
+        "${TOP_DIR}/bin_gppkg_v2/gppkg" install -a ./*.gppkg
+    else
+	gppkg --install ./*.gppkg
+    fi
+}
+
+function uninstall_gppkg() {
+    if [[ ${GP_MAJOR_VERSION} == "7" ]]; then
+        "${TOP_DIR}/bin_gppkg_v2/gppkg" remove -a plr
+    else
+	gppkg --remove plr
+    fi
+}
+
 function _main() {
     source /home/gpadmin/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
 
     local tmp_dir=$(mktemp -d)
     pushd "${tmp_dir}"
     tar xfv /home/gpadmin/bin_plr/plr.tar.gz
-    if [[ ${GP_MAJOR_VERSION} == "7" ]]; then
-        "${TOP_DIR}/bin_gppkg_v2/gppkg" install -a ./*.gppkg
-    else
-	gppkg --install ./*.gppkg
-    fi
+    install_gppkg
     popd
 
-    # FIXME: Remove this when https://github.com/greenplum-db/gpdb/pull/15254 is released
-    sed -i "s|for env.*do|for env in \$(find \"\${GPHOME}/etc/environment.d\" -regextype sed -regex '.*\\\/[0-9][0-9]-.*\\\.conf\$' -type f \| sort -n); do|g" \
-        $GPHOME/greenplum_path.sh
-    source $GPHOME/greenplum_path.sh
+    source "$GPHOME/greenplum_path.sh"
     gpstop -ra
 
     pushd /home/gpadmin/plr_src/src
-
     time make USE_PGXS=1 installcheck
+    popd
+
+    pushd "${tmp_dir}"
+    uninstall_gppkg
+    install_gppkg
     popd
 }
 
