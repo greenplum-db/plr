1a5d5aab10628157ee54cdcf6a54770310f665b5
Chen Mulong <chenmulong@gmail.com>
2023-04-03 15:19:30 +0800

    uninstall

--- regress/sql/uninstall.sql
+++
@@ -1,6 +0,0 @@
-\! gppkg -q --all | sed -n '/^plr-/s|.*|plr|p'
-\! echo '############# SEPARATOR LINE ###########'
--- start_ignore
-\! gppkg -r 'plr-*'
--- end_ignore
-\! gppkg -q --all | sed -n '/^plr-/s|.*|plr|p'
