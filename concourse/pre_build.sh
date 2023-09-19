32bc381a7edcab31ca2ff695d8b543738a6f118a
Chen Mulong <chenmulong@gmail.com>
2023-09-19 14:22:36 +0800

    Move pipelines to gp-extensions-ci (#77)

---
+++ concourse/pre_build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# R will be shipped in the plr gppkg for rhel, since those don't have R in the officail repo.
+# For ubuntu, just use the r-base from apt source.
+function install_build_deps() {
+    case "$OS_NAME" in
+        rhel*)
+            yum install -y gcc-gfortran pcre-devel
+            ;;
+        ubuntu*)
+            apt update
+            DEBIAN_FRONTEND=noninteractive apt install -y r-base
+            ;;
+        *) echo "Unknown OS: $OS_NAME"; exit 1 ;;
+    esac
+}
+
+install_build_deps
